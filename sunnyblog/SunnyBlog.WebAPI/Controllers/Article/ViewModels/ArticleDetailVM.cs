f1aa170ffb29648a52c2c58bdae4c3f8f50d210d
ShiZhaoyang <shizhywin@qq.com>
2023-01-05 03:27:42 -0500

    Add readme

---
+++ sunnyblog/SunnyBlog.WebAPI/Controllers/Article/ViewModels/ArticleDetailVM.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SunnyBlog.WebAPI.Controllers
+{
+    public class ArticleDetailVM
+    {
+        public long Id { get; init; }
+        public string Title { get; init; }
+        public string? Summary { get; init; }
+        public string Content { get; init; }
+        public long ViewCount { get; init; }
+        [JsonIgnore]
+        public long? CategoryId { get; init; }
+        public DateTime Created { get; init; }
+        public string? CategoryName { get; set; }
+        public ArticleDetailVM(long Id, string Title, string? Summary, long ViewCount, long? CategoryId, DateTime Created)
+        {
+            this.Id = Id;
+            this.Title = Title;
+            this.Summary = Summary;
+            this.ViewCount = ViewCount;
+            this.CategoryId = CategoryId;
+            this.Created = Created;
+        }
+    }
+}
