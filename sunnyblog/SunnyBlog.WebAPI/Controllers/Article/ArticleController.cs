da765e8a6f8bc3c032de597296bbdbd32df4f19b
ShiZhaoyang <shizhywin@qq.com>
2023-01-07 21:07:16 -0500

    remove authorize attr

--- sunnyblog/SunnyBlog.WebAPI/Controllers/Article/ArticleController.cs
+++ sunnyblog/SunnyBlog.WebAPI/Controllers/Article/ArticleController.cs
@@ -20,7 +20,6 @@ namespace SunnyBlog.WebAPI.Controllers
             MemoryCache = memoryCache;
         }
         [HttpGet]
-        [Authorize]
         public ActionResult<HotArticleVM[]> GetHotArticles()
         {
             return ArticleRepository.GetHotArticles(e => new HotArticleVM(e.Id, e.Title, e.ViewCount));
